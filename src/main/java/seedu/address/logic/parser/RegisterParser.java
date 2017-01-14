4e96ad478b3d1170444910b5a2deebc8df1da090
MightyCupcakes <gohydj2003@gmail.com>
2017-01-14 10:10:42 +0800

    Modify the register parser annotation to allow passing of parameters to constructor

--- src/main/java/seedu/address/logic/parser/RegisterParser.java
+++ src/main/java/seedu/address/logic/parser/RegisterParser.java
@@ -5,8 +5,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates that the class declaration is intended to be registered as a command parser.
+ * The command parser will be registered to the given command word.
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface RegisterParser {
-    public String commandWord();
+    public String[] commandWord();
+    public boolean passCommandWordAsParam() default false;
 }
