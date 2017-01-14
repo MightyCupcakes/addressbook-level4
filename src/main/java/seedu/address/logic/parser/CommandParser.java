6475ee38cdce5f64ba603e52fe8fc2e687e51100
MightyCupcakes <gohydj2003@gmail.com>
2017-01-14 13:23:10 +0800

    Move common methods and constants into CommandParser

--- src/main/java/seedu/address/logic/parser/CommandParser.java
+++ src/main/java/seedu/address/logic/parser/CommandParser.java
@@ -1,10 +1,17 @@
 package seedu.address.logic.parser;
 
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.IncorrectCommand;
 import seedu.address.logic.parser.ArgumentTokenizer.Prefix;
 
 public abstract class CommandParser {
+
+    public static final Pattern PERSON_INDEX_ARGS_FORMAT = Pattern.compile("(?<targetIndex>.+)");
 
     public static final Prefix PREFIX_PHONE_NUMBER = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
@@ -12,4 +19,22 @@ public abstract class CommandParser {
     public static final Prefix PREFIX_TAGS = new Prefix("t/");
 
     public abstract Command prepareCommand(String args);
+
+    /**
+     * Returns the specified index in the {@code command} if it is a positive unsigned integer
+     * Returns an {@code Optional.empty()} otherwise.
+     */
+    protected Optional<Integer> parseIndex(String command) {
+        final Matcher matcher = PERSON_INDEX_ARGS_FORMAT.matcher(command.trim());
+        if (!matcher.matches()) {
+            return Optional.empty();
+        }
+
+        String index = matcher.group("targetIndex");
+        if (!StringUtil.isUnsignedInteger(index)) {
+            return Optional.empty();
+        }
+        return Optional.of(Integer.parseInt(index));
+
+    }
 }
